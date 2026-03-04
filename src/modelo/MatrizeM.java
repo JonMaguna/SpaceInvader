9f5b472404bb71b421c28a6b192ec999d15c0b6e
JonZalbide <jzalbide002@ikasle.ehu.eus>
2026-03-04 16:42:11 +0100

    SortuMatrizea aldatuta

--- src/modelo/MatrizeM.java
+++ src/modelo/MatrizeM.java
@@ -1,7 +1,9 @@
 package modelo;
 import GelaxkaM;
-import java.util.Observable;
 
+import java.util.ArrayList;
+import java.util.Observable;
+import.java.util.Random;
 public class MatrizeM extends Observable{
 	private MatrizeM nMatrizeM;
 	private GelaxkaM[][] matrizea;
@@ -13,12 +15,25 @@ public class MatrizeM extends Observable{
 		return nMatrizeM;
 	}
 	public void SortuMatrizea() {
+		Random random = new Random();
+		ArrayList<GelaxkaM> Etsaiak = new ArrayList<>();
 		for (int i = 0; i<100; i++) {
 			for (int j=0;j<60;j++) {
 				matrizea[i][j] = new GelaxkaM();
+				if (i%5 = 4 && j=5) {
+					Etsaiak.add(matrizea[i][j]);
+				}
+				
 			}
 		}
-	}
+		int EtsaienKantitatea = random.nextInt(5) + 4; 
+        
+        Collections.shuffle(Etsaiak);
+        for (int k=0; k<EtsaienKantitatea;k++) {
+        	Etsaiak.get[k].entitate = ETSAIA;
+        }
+		matrizea[55][50].entitate = ESPAZIONTZI;
+	}	
 	public void mugituOntzia (int Mugimendua) {
 		
 	}
