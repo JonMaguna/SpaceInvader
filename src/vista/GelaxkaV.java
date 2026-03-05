905c6133f846d8ae0554079ab6ce419d6cceb1e2
ComradeTxuria <sortega024@ikasle.ehu.eus>
2026-03-05 12:51:50 +0100

    GelaxkaV eta MatrizeV konponduta

--- src/vista/GelaxkaV.java
+++ src/vista/GelaxkaV.java
@@ -1,42 +1,11 @@
 package vista;
-//import modelo.Bala;
-import modelo.Espaziontzia;
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
 
-<<<<<<< HEAD
-public class GelaxkaV extends JLabel implements Observer {
 
-    private static final long serialVersionUID = 1L;
-    private int Xpos;
-    private int Ypos;
-
-    public GelaxkaV(String texto, int x, int y, Espaziontzia perts) {
-        super(texto);
-        this.Xpos = x;
-        this.Ypos = y;
-        
-        perts.addObserver(this);
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-        
-        Espaziontzia espaziontzia = (Espaziontzia) o;
-        //Bala bala= (Bala) o;
-        
-        if (this.Xpos == espaziontzia.getXposizioa() && this.Ypos == espaziontzia.getYposizioa()) {
-            this.setBackground(Color.MAGENTA);
-        /*} else if(this.miX == bala.getX() && this.miY == bala.getY()){
-            this.setBackground(Color.WHITE);  // La nave no está, soy espacio vacío
-        */}else {
-        	this.setBackground(Color.BLACK);
-        }
-    }
-}
-=======
 import modelo.EntitateMota;
 
 
@@ -79,5 +48,3 @@ public class GelaxkaV extends JLabel{
 	
 	
 }
-
->>>>>>> 95dbae67b4c7f3da4a5bd4896c0a050391de1d32
