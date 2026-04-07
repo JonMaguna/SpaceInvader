dca118e2e0886bce9ecffa91fdc12a233d4f4d83
JonZalbide <jzalbide002@ikasle.ehu.eus>
2026-04-07 18:03:50 +0200

    Etsaiak hiltzen dira

--- src/modelo/Espaziontzi.java
+++ src/modelo/Espaziontzi.java
@@ -26,49 +26,29 @@ public class Espaziontzi extends Entitate {
 	}
 	
 	public boolean mugituDaiteke(Mugimendua mugimendua) {
-		boolean mugitu = true;
-		EntitateMota entitatea = null;
-		int hurrengoaX = this.koordenatu[0][0];
-		int hurrengoaY = this.koordenatu[0][1];
-		switch (mugimendua) {
-		case GORA:
-			if(this.koordenatu[0][1] == 0) {mugitu = false;} 
-			else {
-				entitatea = MatrizeM.getnMatrizeM().zerDago(this.koordenatu);
-			}
-			break;
-		case BEHERA:
-			if(this.koordenatu[0][1] == 59) {mugitu = false;} 
-			else {
-				entitatea = MatrizeM.getnMatrizeM().zerDago(this.koordenatu);
-			}
-			break;
-		case EZKERRA:
-			if(this.koordenatu[0][0] == 0) {mugitu = false;} 
-			else {
-				entitatea = MatrizeM.getnMatrizeM().zerDago(this.koordenatu);
-			}
-			break;
-		case ESKUMA:
-			if(this.koordenatu[0][0] == 99) {mugitu = false;} 
-			else {
-				entitatea = MatrizeM.getnMatrizeM().zerDago(this.koordenatu);
-			}
-			break;
-		default:
-			break;
-		}
-		if(mugitu && entitatea != null) { 
-			switch (entitatea) {
-			case ETSAIA:
-				mugitu = false;
-				setBizirik(false);
-				break;
-			default:			
-				break;
-			}
-		}
-		return mugitu;
+	    boolean mugitu = true;
+	    int xHurrengoa = this.koordenatu[0][0];
+	    int yHurrengoa = this.koordenatu[0][1];
+
+	    switch (mugimendua) {
+	        case GORA:    yHurrengoa--; break;
+	        case BEHERA:  yHurrengoa++; break;
+	        case EZKERRA: xHurrengoa--; break;
+	        case ESKUMA:  xHurrengoa++; break;
+	    }
+
+	    if (xHurrengoa < 0 || xHurrengoa > 99 || yHurrengoa < 0 || yHurrengoa > 59) {
+	        return false;
+	    }
+	    int[][] posHurrengoa = {{xHurrengoa, yHurrengoa}};
+	    EntitateMota entitatea = MatrizeM.getnMatrizeM().zerDago(posHurrengoa);
+
+	    if (entitatea == EntitateMota.ETSAIA) {
+	        this.setBizirik(false); 
+	        return false;           
+	    }
+
+	    return true;
 	}
 	
 	public void mugitu(Mugimendua mugimendu) {
--- src/modelo/EspaziontziNodo.java
+++ src/modelo/EspaziontziNodo.java
@@ -11,34 +11,29 @@ public abstract class EspaziontziNodo extends Entitate{
 	public ArrayList<Entitate> getGelaxkak() {
 		return this.gelaxkak;
 	}
-	
 	public void mugitu(Mugimendua mugimendu) {
-		boolean mugituDaiteke = true;
-		int i = 0;
-		while(i < this.gelaxkak.size() && mugituDaiteke){
-			mugituDaiteke = this.gelaxkak.get(i).mugituDaiteke(mugimendu);
-			i++;
-		} 
-		if(mugituDaiteke) {
-			MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, 0, EntitateMota.HUTSA);
-			for (Entitate pixel : this.gelaxkak) {
-				pixel.mugitu(mugimendu);
-			}
-			MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, id, EntitateMota.ESPAZIONTZI);
-			eguneratuPosizioNagusia(mugimendu);
-		}else {
-			if (!this.gelaxkak.get(i - 1).bizirik()) {
-	            
-	            this.setBizirik(false); 
-	      
-	            MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, 0, EntitateMota.HUTSA);
-	            for (Entitate pixel : this.gelaxkak) {
-	                pixel.setBizirik(false);
-	            }	            
-	            MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, id, EntitateMota.ESPAZIONTZI);
-	        }
+		    boolean mugituDaiteke = true;
+		    Entitate Hurrengoa = null;
+		    for (Entitate pixel : this.gelaxkak) {
+		        if (!pixel.mugituDaiteke(mugimendu)) {
+		            mugituDaiteke = false;
+		            Hurrengoa = pixel;
+		            break;
+		        }
+		    }
+		    if (mugituDaiteke) {
+		        MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, 0, EntitateMota.HUTSA);
+		        for (Entitate pixel : this.gelaxkak) {
+		            pixel.mugitu(mugimendu);
+		        }
+		        MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, id, EntitateMota.ESPAZIONTZI);
+		        eguneratuPosizioNagusia(mugimendu);
+		    } else {
+		        if (Hurrengoa!= null && !Hurrengoa.bizirik()) {
+		            this.setBizirik(false); 
+		        }
+		    }
 		}
-	}
 	protected void eguneratuPosizioNagusia(Mugimendua m) {
 		switch(m) {
             case EZKERRA: this.x--; break;
