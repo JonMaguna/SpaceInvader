3d394fcd5b05b42e6b1f064c761f0d8c814fd2fa
JonZalbide <jzalbide002@ikasle.ehu.eus>
2026-04-09 18:15:50 +0200

    Gezi Bala eginda

--- src/modelo/BalaFactory.java
+++ src/modelo/BalaFactory.java
@@ -17,7 +17,7 @@ public class BalaFactory {
 		case BALA_NORMALA:
 			return new BalaNodo(x, y, new int[][]{{x, y}}, id);
 		case BALA_AZKARRA:
-			return new BalaNodo(x, y, new int[][]{{x, y}, {x-1, y-1}, {x+1, y+1}}, id);
+			return new BalaNodo(x, y, new int[][]{{x, y}, {x-1, y+1}, {x+1, y+1}}, id);
 		case BALA_HANDIA:
 			return new BalaNodo(x, y, new int[][]{{x, y}, {x, y-1}, {x+1, y-1}, {x-1, y-1}, 
 				{x, y-2}, {x+1, y-2}, {x+2, y-2}, {x-1, y-2}, {x-2, y-2}, {x, y-3}, {x+1, y-3}, {x-1, y-3}, {x, y-4}}, id);
