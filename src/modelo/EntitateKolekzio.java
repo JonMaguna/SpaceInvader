308f06b5b5412e7a59a12d047a953ce7714bc6e3
JonZalbide <jzalbide002@ikasle.ehu.eus>
2026-04-09 17:32:13 +0200

    Etsaiak ez dira ikutzen

--- src/modelo/EntitateKolekzio.java
+++ src/modelo/EntitateKolekzio.java
@@ -68,10 +68,10 @@ public class EntitateKolekzio {
 			int nora = new Random().nextInt(3);
 			Entitate etsai = this.mapa.get(EntitateMota.ETSAIA).get(i);
 			switch(nora) {
-			case 0: 
+			/*case 0: 
 				etsai.mugitu(Mugimendua.BEHERA);
 				break;
-			case 1:
+			*/case 1:
 				etsai.mugitu(Mugimendua.EZKERRA);
 				break;
 			case 2:
@@ -79,16 +79,15 @@ public class EntitateKolekzio {
 				break;
 			}
 			if(!etsai.bizirik()) {
-				if(!etsai.bizirik) {
 				    ArrayList<Entitate> Etsaia = ((EtsaiNodo) etsai).getGelaxkak();
 				    MatrizeM.getnMatrizeM().gelaxkakAktualizatu(Etsaia, 0, EntitateMota.HUTSA);
 				    this.mapa.get(EntitateMota.ETSAIA).remove(i);
+				    i--;
 				    if(this.mapa.get(EntitateMota.ETSAIA).size() == 0) {
 				        JokoKudeatzailea.getnJokoKudeatzailea().jokoaGelditu(2);
 				    }
 				}
 			}
-		}
 	}
 	
 	public void mugituOntzia (Mugimendua mugimendua) {
@@ -102,7 +101,7 @@ public class EntitateKolekzio {
 		 boolean pasaDa = false;
 		 long orain = System.currentTimeMillis();
 		 
-		 if (orain - this.azkenMugimendua < 125) {
+		 if (orain - this.azkenMugimendua < 130) {
 			 pasaDa = true;
 		 }
 		 else {
--- src/modelo/Etsaiak.java
+++ src/modelo/Etsaiak.java
@@ -5,17 +5,16 @@ public class Etsaiak extends Entitate {
 	public Etsaiak(int x, int y, int id) {
 		super(x, y, id, true);
 	}
-
 	public boolean mugituDaiteke(Mugimendua mugimendu) {
 		boolean mugitu = true;
 		EntitateMota entitatea = null;
 		int xHurrengoa = this.koordenatu[0][0];
 		int yHurrengoa = this.koordenatu[0][1];
-		
 		switch(mugimendu) {
 	        case BEHERA:  yHurrengoa++; break;
 	        case EZKERRA: xHurrengoa--; break;
 	        case ESKUMA:  xHurrengoa++; break;
+	        default: break;
 		}
 		if (yHurrengoa > 59) {
 			JokoKudeatzailea.getnJokoKudeatzailea().jokoaGelditu(1);
@@ -27,27 +26,54 @@ public class Etsaiak extends Entitate {
 		int[][] Hurrengoa = {{xHurrengoa, yHurrengoa}};
 		entitatea = MatrizeM.getnMatrizeM().zerDago(Hurrengoa);
 		int BesteId = MatrizeM.getnMatrizeM().zeinIDDago(Hurrengoa);
-		switch(entitatea) {
-		case ESPAZIONTZI:
-			EntitateKolekzio.getnPertsonaiZerrenda().setBizirik(EntitateMota.ESPAZIONTZI, 1, false);
-            mugitu = false;
-            break;
-		case BALA:
-			EntitateKolekzio.getnPertsonaiZerrenda().setBizirik(EntitateMota.ETSAIA, this.id, false);
-			this.setBizirik(false);
-			mugitu = false;
-			break;
-		case ETSAIA:
-			if(BesteId != this.id) {
-                mugitu = false; 
-            }
-			break;
-		default:
-			break;
+		
+		if (entitatea != null) {
+			switch(entitatea) {
+			case ESPAZIONTZI:
+				EntitateKolekzio.getnPertsonaiZerrenda().setBizirik(EntitateMota.ESPAZIONTZI, 1, false);
+	            mugitu = false;
+	            break;
+			case BALA:
+				EntitateKolekzio.getnPertsonaiZerrenda().setBizirik(EntitateMota.ETSAIA, this.id, false);
+				this.setBizirik(false);
+				mugitu = false;
+				break;
+			case ETSAIA:
+				if(BesteId != this.id) {
+	                mugitu = false; 
+	            }
+				break;
+			default:
+				break;
+			}
+		}
+		if (mugitu) {
+			switch(mugimendu) {
+		    case BEHERA:  Hurrengoa[0][1]++; break;
+		    case EZKERRA: Hurrengoa[0][0]--; break;
+		    case ESKUMA:  Hurrengoa[0][0]++; break;
+		    default: break;
+			}
+			
+			if (Hurrengoa[0][1] <= 59 && Hurrengoa[0][0] >= 0 && Hurrengoa[0][0] <= 99) {
+				entitatea = MatrizeM.getnMatrizeM().zerDago(Hurrengoa);
+				BesteId = MatrizeM.getnMatrizeM().zeinIDDago(Hurrengoa);
+				
+				if(entitatea != null) {
+					switch(entitatea) { 
+					case ETSAIA:
+						if(BesteId != this.id) {
+				            mugitu = false; 
+				        }
+						break;
+					default:
+						break;
+					}
+				}
+			}
 		}
 		return mugitu;
 	}
-	
 	public void mugitu(Mugimendua mugimendu) {
 		switch(mugimendu) {
 		case BEHERA:
