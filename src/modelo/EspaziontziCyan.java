d9a215de41563d55f86038ed7a4ad1ab588dae4a
JonZalbide <jzalbide002@ikasle.ehu.eus>
2026-04-09 18:47:31 +0200

    Espaziontziak eginda

--- src/modelo/EspaziontziCyan.java
+++ src/modelo/EspaziontziCyan.java
@@ -7,7 +7,7 @@ public class EspaziontziCyan extends EspaziontziNodo {
 	
 	public EspaziontziCyan(int x, int y, int id) {
 		super(x, y, id);
-        int[][] forma = {{0,0},{2,0},{0,1},{1,1},{2,1},{0,2},{1,2},{2,2}};
+        int[][] forma = {{-1,0},{1,0},{-1,1},{0,1},{1,1},{-1,2},{0,2},{1,2}};
 		for (int[] p: forma) {
 			this.gelaxkak.add(new Espaziontzi(x+p[0], y+p[1], id, Color.CYAN));
 		}
@@ -25,9 +25,9 @@ public class EspaziontziCyan extends EspaziontziNodo {
 	public BalaNodo tiroEgin(int id) {
 		switch(this.bala) {
 		case 1:
-			return BalaFactory.getnBalaFactory().sortuBala(this.x+1, this.y - 2, id, BalaMota.BALA_NORMALA);
+			return BalaFactory.getnBalaFactory().sortuBala(this.x, this.y - 2, id, BalaMota.BALA_NORMALA);
 		case 2:
-			return BalaFactory.getnBalaFactory().sortuBala(this.x+1, this.y - 2, id, BalaMota.BALA_HANDIA);
+			return BalaFactory.getnBalaFactory().sortuBala(this.x, this.y - 2, id, BalaMota.BALA_HANDIA);
 		default:
 			return null;
 		}
