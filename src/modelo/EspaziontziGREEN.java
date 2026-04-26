9e6cae51e2f6a3062e141f621624be479a57d399
JonZalbide <jzalbide002@ikasle.ehu.eus>
2026-04-26 11:49:00 +0200

    Espaziontzi Green bizitzak ditu

--- src/modelo/EspaziontziGREEN.java
+++ src/modelo/EspaziontziGREEN.java
@@ -3,7 +3,8 @@ package modelo;
 import java.awt.Color;
 
 public class EspaziontziGREEN  extends EspaziontziNodo{
-
+	private int bizitzak = 3;
+	private long azkenKolpea = 0;
 	protected EspaziontziGREEN(int x, int y, int id) {
 		super(x,y,id);
 		this.kolorea = Color.GREEN;
@@ -15,5 +16,25 @@ public class EspaziontziGREEN  extends EspaziontziNodo{
 		}
 		MatrizeM.getnMatrizeM().gelaxkakAktualizatu(gelaxkak, id, EntitateMota.ESPAZIONTZI);
 	}
-	
+	@Override
+    public void setBizirik(boolean b) {
+        if (!b) { 	
+        	long orain = System.currentTimeMillis();
+        	if (orain - this.azkenKolpea < 500) {
+            return; 
+        }
+        	this.azkenKolpea = orain; 
+            this.bizitzak--;
+            if (this.bizitzak <= 0) {
+                super.setBizirik(false);
+            }else {
+            	this.StepBack();
+            }
+        }
+    }
+	private void StepBack() {
+		for (int i = 0; i < 5; i++) {
+			this.mugitu(Mugimendua.BEHERA);
+		}
+	}
 }
